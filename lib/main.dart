34c27b3d2a6c96166637503bd0ccbdfb416e7dab
juanmanuel0963 <juanmanuel0963@gmail.com>
2022-05-21 15:18:27 -0500

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:auth_firebase/auth/controllers/auth_controller.dart';
 import 'package:auth_firebase/auth/screens/splash_screen.dart';
 import 'package:auth_firebase/helpers/loading/loading_overlay.dart';
 import 'package:auth_firebase/helpers/translations/app_translations.dart';
+import 'package:auth_firebase/helpers/routes/routes.dart';
+import 'package:auth_firebase/home/screens/home_screen.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -33,10 +36,38 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      //home: SplashScreen(),
       home: LoadingOverlay(
         child: SplashScreen(),
       ),
+      builder: (context, child) => ResponsiveWrapper.builder(
+          BouncingScrollWrapper.builder(context, child!),
+          maxWidth: 1200,
+          minWidth: 450,
+          defaultScale: true,
+          breakpoints: [
+            const ResponsiveBreakpoint.resize(450, name: MOBILE),
+            const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+            const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+            const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+          ],
+          background: Container(color: const Color(0xFFF5F5F5))),
+      //initialRoute: Routes.splash,
+      onGenerateRoute: (RouteSettings settings) {
+        return Routes.fadeThrough(settings, (context) {
+          switch (settings.name) {
+            case Routes.home:
+              return const HomeView();
+            case Routes.splash:
+              return SplashScreen();
+            case Routes.settings:
+              return SplashScreen();
+            default:
+              return const SizedBox.shrink();
+          }
+        });
+      },
+      debugShowCheckedModeBanner: false,
     );
   }
 }
